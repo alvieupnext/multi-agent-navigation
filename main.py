85a1ee67d700798de88552dbc2ac53ab932c087c
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2023-12-26 17:02:05 +0100

    Implemented main loop

--- main.py
+++ main.py
@@ -1,33 +1,113 @@
-from environments.five_grid import FiveGrid, layouts
+from environments.five_grid import FiveGrid, layouts as env_layouts
+from agents.Sender import Sender
+from agents.Receiver import Receiver
 import numpy as np
 
-chosen_layout = layouts["pong"]
+chosen_layout = env_layouts["pong"]
 env = FiveGrid(illegal_positions=chosen_layout)
 
-#Generate a generic loop for the environment using an agent that does random actions
-# Path: main.py
-# Compare this snippet from agents/Receiver.py:
-def choose_action(action_mask):
-    # Choose a random action
-    action = np.random.choice(np.where(action_mask == 1)[0])
-    # Return the action
-    return action
+# The hyperparameters
+# Amount of sender agents
+M_values = [1, 2, 3, 4, 5]
+# Channel capacity (amount of messages each sender can send)
+C_values = [3, 4, 5, 8, 9, 16, 25, 27, 32, 36, 64]
+# The learning rate for RMSprop, which is an optimization algorithm used to adjust weights in the learning process.
+# Listed but not used in the paper: 5e-5
+eta_values = [1e-4, 5e-4, 1e-3]
+# The epsilon values for the sender
+# Listed but not used in the paper: 0.1, 0.15
+epsilon_s_values = [0.01, 0.05]
+# The epsilon values for the receiver
+# Listed but not used in the paper: 0.15
+epsilon_r_values = [0.01, 0.05, 0.1]
+# The receiver's Q-learning discount factor
+gamma_values = [0.7, 0.8, 0.9]
+# The possible layouts for the environment
+layouts = [env_layouts["pong"], env_layouts["four_room"], env_layouts["two_room"], env_layouts["flower"], env_layouts["empty_room"]]
 
-# Termination probability of the environment
-options = {"termination_probability": 0.1}
+# 12 million steps
+learning_steps = 12000000
+# # Amount of episodes
+# episodes =
 
-observations, infos = env.reset(options=options)
+for lay_out in layouts:
+    env = FiveGrid(illegal_positions=lay_out)
+    for gamma in gamma_values:
+        options = {"termination_probability": 1 - gamma}
+        for M in M_values:
+            for C in C_values:
+                for eta in eta_values:
+                    for epsilon_s in epsilon_s_values:
+                        senders = [Sender(epsilon_s, C, env.world_size) for _ in range(M)]
+                        for epsilon_r in epsilon_r_values:
+                            print("M: " + str(M) + ", C: " + str(C) + ", eta: " + str(eta) + ", epsilon_s: " + str(
+                                epsilon_s) + ", epsilon_r: " + str(epsilon_r) + ", gamma: " + str(gamma))
+                            receiver = Receiver(epsilon_r, env.world_size, M, C)
+                            action = None
+                            messages = []
+                            observations, infos = env.reset(options=options)
+                            # Get the goal state
+                            goal_state = env.returnGoal()
+                            messages = []
+                            # For every sender, generate a message
+                            for sender in senders:
+                                messages.append(sender.choose_action(goal_state))
+                            for step in range(learning_steps):
+                                observation = observations["receiver"]
+                                # Choose an appropriate action for the receiver
+                                action = receiver.choose_action([observation["observation"]].extend(messages), observation["action_mask"])
+                                # Get the observations, rewards, terminations, truncations and infos
+                                next_observations, rewards, terminations, truncations, infos = env.step({"receiver": action})
+                                reward = rewards["receiver"]
+                                # Learn from the experience as a receiver
+                                next_observation = next_observations["receiver"]
+                                receiver.learn([observation["observation"]].extend(messages), action, reward,
+                                               [next_observation["observation"]].extend(messages))
+                                # Check for termination or truncation
+                                if terminations["receiver"] or truncations["receiver"]:
+                                    # Senders all have an opportunity to learn
+                                    for sender, message in zip(senders, messages):
+                                        # TODO: flatten the location
+                                        sender.learn(goal_state, message, reward)
+                                    # Reset the environment
+                                    observations, infos = env.reset(options=options)
+                                    # Get the goal state
+                                    goal_state = env.returnGoal()
+                                    messages = []
+                                    # For every sender, generate a message
+                                    for sender in senders:
+                                        messages.append(sender.choose_action(goal_state))
+                                else:
+                                    observations = next_observations
 
-env.render()
 
-while env.agents:
-    # From observations, get the action mask
-    action_mask = observations["receiver"]["action_mask"]
-    choice = choose_action(action_mask)
-    # Get the observations, rewards, terminations, truncations and infos
-    observations, rewards, terminations, truncations, infos = env.step({"receiver": choice})
-    # Print receiver reward
-    print("Receiver reward: " + str(rewards["receiver"]))
-    # Render the environment
-    env.render()
+
+
+
+# #Generate a generic loop for the environment using an agent that does random actions
+# # Path: main.py
+# # Compare this snippet from agents/Receiver.py:
+# def choose_action(action_mask):
+#     # Choose a random action
+#     action = np.random.choice(np.where(action_mask == 1)[0])
+#     # Return the action
+#     return action
+#
+# # Termination probability of the environment
+# options = {"termination_probability": 0.1}
+#
+# observations, infos = env.reset(options=options)
+#
+# env.render()
+#
+# while env.agents:
+#     # From observations, get the action mask
+#     action_mask = observations["receiver"]["action_mask"]
+#     choice = choose_action(action_mask)
+#     # Get the observations, rewards, terminations, truncations and infos
+#     observations, rewards, terminations, truncations, infos = env.step({"receiver": choice})
+#     # Print receiver reward
+#     print("Receiver reward: " + str(rewards["receiver"]))
+#     # Render the environment
+#     env.render()
 
