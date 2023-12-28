1ebc1549fe77a84bd06d586da5fdd7c22ead97c6
alexandre-szomonyak <alexandreszomonyak@hotmail.com>
2023-12-28 15:32:21 +0100

    making a tabular version of the receiver

--- agents/Receiver.py
+++ agents/Receiver.py
@@ -4,70 +4,26 @@ import numpy as np
 
 class Receiver:
 
-    def __init__(self, discount_factor, epsilon, world_size, num_senders, channel_capacity, eta):
+    def __init__(self, world_size, channel_capacity, discount_factor, learning_rate, epsilon):
+        self.number_of_directions = 4
+        self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.epsilon = epsilon
-        self.observation_size = world_size+num_senders*channel_capacity
-        self.number_of_directions = 4
-        optimizer = tf.keras.optimizers.legacy.RMSprop(learning_rate=eta)
-        self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(self.number_of_directions, activation='softmax', input_shape=(self.observation_size,))
-        ])
-        self.model.compile(optimizer=optimizer, loss='mse')
-        # Buffer that will contain the examples seen after each step of an episode. At the end of the episode, the
-        # examples are taken from the buffer and used to update the model in batch.
-        self.buffer = []
+        self.channel_capacity = channel_capacity
+        self.q_table = np.zeros((world_size, self.channel_capacity, self.number_of_directions))
 
-    def choose_action(self, observation, action_mask):
 
-        # If a random number is smaller than epsilon => take a random direction (exploration)
+    def choose_action(self, current_state, message, action_mask):
         if np.random.rand() < self.epsilon:
             action_mask_array = np.array(action_mask)
             legal_actions = np.where(action_mask_array == 1)[0]
             return np.random.choice(legal_actions)
-        # Otherwise, follow the model to predict the next move of the agent (exploitation)
         else:
-            prediction = self.model.predict(np.reshape(observation, (1, self.observation_size)),verbose=0)
-            prediction = np.reshape(prediction, (self.number_of_directions,))
-            # Set the probability of the unavailable actions to -inf
-            prediction[action_mask == 0] = 0
-            return np.argmax(prediction)
+            actions = self.q_table[current_state, message]
+            actions[action_mask == 0] = -1
+            return np.argmax(actions)
 
-    def learn(self):
-        """
-        Updates the model with the examples in the buffer
-        """
-        # Get the observations and outputs from the buffer
-        observations, outputs = zip(*self.buffer)
-        # Concatenate the observations and outputs
-        observations = np.concatenate(observations)
-        outputs = np.concatenate(outputs)
-        # Train the model on the batch
-        self.model.fit(observations, outputs, verbose=0)
-        # Empty the buffer
-        self.buffer = []
-
-    def add_example(self, observation, next_observation, action, reward):
-        """
-        Adds an example to the buffer
-        """
-        output = np.zeros(self.number_of_directions)
-        max_q = np.argmax(self.model.predict(np.reshape(next_observation, (1, self.observation_size)), verbose=0))
-        output[action] = reward + (self.discount_factor * max_q)
-        # Reshape the context vector
-        observation = np.reshape(observation, (1, self.observation_size))
-        # Reshape the output vector
-        output = np.reshape(output, (1, self.number_of_directions))
-        self.buffer.append((observation, output))
-
-if __name__ == "__main__":
-    receiver = Receiver(0.1, 0.9, 25, 1, 3)
-    # in a for loop, generate a random context vector, choose an action, learn. Print the weights of the model
-    for i in range(100):
-        observation = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-        action_mask = [0, 1, 1, 0]
-        action = receiver.choose_action(observation, action_mask)
-        new_observation = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-        reward = 0
-        receiver.learn(observation, new_observation, action, reward)
-        print(receiver.model.get_weights())
+    def learn(self, current_state, message, action, reward, next_state):
+        prediction = self.q_table[current_state, message, action]
+        target = reward + self.discount_factor * np.max(self.q_table[next_state, message])
+        self.q_table[current_state, message, action] += self.learning_rate * (target - prediction)
