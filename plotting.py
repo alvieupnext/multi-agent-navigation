b713aa608c52221738665df59f34c0c11e93d917
LennertSaerens <lennert.saerens@gmail.com>
2024-01-02 10:36:17 +0100

    Updated plotting code to show walls

--- plotting.py
+++ plotting.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as mcolors
 
+
 def plot_reward_curves(pdDataframe):
     """
     For each of the environments, plot the reward curves for the different settings.
@@ -11,9 +12,11 @@ def plot_reward_curves(pdDataframe):
     for env in environments:
         plot_reward_curve(pdDataframe[pdDataframe["Env"] == env], env)
 
+
 environments = ["pong", "four_room", "two_room", "flower", "empty_room"]
 
 gamma = 0.8
+
 
 def plot_reward_curve(pdDataframe, environment, gamma=0.99):
     settings = ["5S-1R", "4S-1R", "3S-1R", "2S-1R", "1S-1R", "Random", "Q-learning", "Max"]
@@ -53,7 +56,8 @@ def plot_average_return_per_channel_capacity_excluding(pdDataframe, environments
         grouped_data = env_data.groupby('C')['DiscountedReward'].mean().reset_index()
 
         # Plotting the average return for the current environment
-        ax.plot(grouped_data.index, grouped_data['DiscountedReward'], marker='o', linestyle='-', label="Average excluding specified settings") # Using index for equal spacing
+        ax.plot(grouped_data.index, grouped_data['DiscountedReward'], marker='o', linestyle='-',
+                label="Average excluding specified settings")  # Using index for equal spacing
 
         ax.set_title(environment)
         ax.set_xlabel("Channel Capacity")
@@ -67,6 +71,7 @@ def plot_average_return_per_channel_capacity_excluding(pdDataframe, environments
 
     plt.tight_layout()
     plt.show()
+
 
 # print("before reading")
 # Open tabular_results_pong_0.0001_0.05_0.05_0.8_12000000 from results folder
@@ -127,29 +132,37 @@ for env in environments:
 # Reset index
 df = df.reset_index(drop=True)
 
-print(df)
+# print(df)
 
 # Calculate the discounted rewards and add them to the DataFrame
 df['DiscountedReward'] = df['Reward'] * gamma ** df['Steps']
 
+
 # Plot the reward curves
 # plot_reward_curves(df)
 
-plot_average_return_per_channel_capacity_excluding(df, environments, ["Random", "Q-learning", "Max"])
+# plot_average_return_per_channel_capacity_excluding(df, environments, ["Random", "Q-learning", "Max"])
 
-def visualize_belief(belief_table):
+def visualize_belief(belief_table, layout):
     indices = np.argmax(belief_table, axis=1)
+    # get max index
+    wall = np.max(indices) + 1
     indices = indices.reshape((5, 5))
+    print(indices)
     # display the value in each cell
     for i in range(5):
         for j in range(5):
-            plt.text(j, i, indices[i, j], ha='center', va='center', color='black')
+            if (j, i) in layout:
+                indices[i, j] = wall
+            else:
+                plt.text(j, i, indices[i, j], ha='center', va='center', color='black')
     plt.imshow(indices)
     plt.colorbar()
     plt.show()
 
-def visualize_receiver_policy(q_table, world_size, channel_capacity, message):
-    dimensions = int(world_size**(1/2))
+
+def visualize_receiver_policy(q_table, world_size, channel_capacity, message, layout):
+    dimensions = int(world_size ** (1 / 2))
     policy_table = np.argmax(q_table[:, message, :], axis=1).reshape((dimensions, dimensions))
     directions = {
         0: (0, -0.3),  # Up
@@ -157,14 +170,19 @@ def visualize_receiver_policy(q_table, world_size, channel_capacity, message):
         2: (0.3, 0),  # Right
         3: (-0.3, 0)  # Left
     }
+    wall = 4
 
     for i in range(dimensions):
         for j in range(dimensions):
-            dx, dy = directions[policy_table[i, j]]
-            plt.arrow(j-dx, i-dy, dx*2, dy*2, head_width=0.1, head_length=0.1, fc='k', ec='k')
+            if (j, i) in layout:
+                policy_table[i, j] = wall
+            else:
+                dx, dy = directions[policy_table[i, j]]
+                plt.arrow(j - dx, i - dy, dx * 2, dy * 2, head_width=0.1, head_length=0.1, fc='k', ec='k')
     plt.imshow(policy_table)
     plt.title("C = %s" % channel_capacity)
     plt.show()
+
 
 # Example usage (assuming you have a Q-table named q_table):
 # visualize_q_table(q_table)
@@ -172,4 +190,8 @@ def visualize_receiver_policy(q_table, world_size, channel_capacity, message):
 # Example for visualising the receiver's policy given a q-table with random values
 # q_table = np.random.randint(0, 20, (25, 3, 4))
 # print(str(q_table))
-# visualize_receiver_policy(q_table, 25, 3, 2)
+# visualize_receiver_policy(q_table, 25, 3, 2, [(0, 1), (0, 2), (0, 3), (2, 0), (2, 1), (2, 3), (2, 4), (4, 1), (4, 2), (4, 3)])
+
+# Example for visualising the belief table given a belief table of 25 states and 3 messages
+# belief_table = np.random.randint(0, 4, (25, 3))
+# visualize_belief(belief_table, [(0, 1), (0, 2), (0, 3), (2, 0), (2, 1), (2, 3), (2, 4), (4, 1), (4, 2), (4, 3)])
