b9f29f69f276ec20ffff78a5034002717394da99
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2024-01-05 17:55:36 +0100

    Fixed unpacking bug

--- MeanTrainingReturns.py
+++ MeanTrainingReturns.py
@@ -27,7 +27,7 @@ def run_training(type, C, eta, epsilon_max, epsilon_min, epsilon_decay, gamma, l
     M = int(type[0])
     num_messages = int(C ** (1 / M))
     # Run the experiment
-    rewards, steps, total_steps = run_experiment(M, num_messages, eta, epsilon_max, epsilon_min, epsilon_decay, gamma, env, learning_steps)
+    rewards, steps, total_steps, _ = run_experiment(M, num_messages, eta, epsilon_max, epsilon_min, epsilon_decay, gamma, env, learning_steps)
   # Generate a type from the M value
   # Create a dataframe from the results
   df = pd.DataFrame(list(zip(rewards, steps, total_steps)), columns=["Reward", "Steps", "TotalSteps"])
@@ -62,7 +62,7 @@ possible_C_values = {
     5: [32]
 }
 # Learning steps is 4 million
-learning_steps = 4_000_000
+learning_steps = 4_000
 # The possible layouts for the environment
 # layout = "four_room"
 sender_receiver = ["5S-1R", "4S-1R", "3S-1R", "2S-1R", "1S-1R"]
@@ -78,8 +78,8 @@ if __name__ == "__main__":
   else:
     layout = "two_room"  # Default environment if none is provided
   print(f"Now working on {layout}")
-  ray.init(address='auto')
-  # ray.init()
+  # ray.init(address='auto')
+  ray.init()
   remotes = []
   for type in types:
     # Get the first character of the type and convert it to an integer
